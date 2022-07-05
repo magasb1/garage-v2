cb40ba8913276c87081f731807934da14bdb0520
Magnus A <magnus@h01.int>
2022-07-05 20:11:48 +0200

    oppdatert

--- server/server.js
+++ server/server.js
@@ -12,8 +12,9 @@ app.get("/", (req, res) => {
 });
 
 
+const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening on ${HOST + ':' + PORT}`);
 });
