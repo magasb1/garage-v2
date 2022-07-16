aec8f58f7424d56873f71be00751f774fc7769de
Magnus A <magnus@h01.int>
2022-07-16 23:51:56 +0200

    oppdatert

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
-    type: "sqlite",
+    type: "better-sqlite3",
     database: "./config/db/database.sqlite",
     synchronize: true,
     logging: false,
